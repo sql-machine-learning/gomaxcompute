c7abf141e65dab93b4b265750d6dc9d749cbed3e
Yan Xu <yancey1989@gmail.com>
2019-07-29 18:40:58 +0800

    fetch credientical from Driver.cfg (#47)

--- driver.go
+++ driver.go
@@ -12,12 +12,14 @@ func init() {
 }
 
 // impls database/sql/driver.Driver
-type Driver struct{}
+type Driver struct {
+	cfg *Config
+}
 
 func (d Driver) Open(dsn string) (driver.Conn, error) {
-	cfg, err := ParseDSN(dsn)
-	if err != nil {
+	var err error
+	if d.cfg, err = ParseDSN(dsn); err != nil {
 		return nil, err
 	}
-	return &odpsConn{&http.Client{}, cfg}, nil
+	return &odpsConn{&http.Client{}, d.cfg}, nil
 }
